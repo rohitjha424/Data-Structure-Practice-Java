d44cf6319fac08d9686185732785118074938426
rohitjha424 <rohitjha424@gmail.com>
2026-03-08 23:48:10 +0530

    Character Array

---
+++ New2026/String/StringBasics.java
@@ -0,0 +1,20 @@
+package New2026.String;
+
+public class StringBasics {
+
+    public static void main (String[] args){
+        //character Array of size 5
+        char[] arr = new char[5];
+        arr[0] = 'a';
+        arr[1] = 'b';
+        arr[2] = 'c';
+        arr[3] = 'd';
+        arr[4] = 'e';
+
+        for (char c : arr) {
+            System.out.println(c);
+        }
+
+
+    }
+}
