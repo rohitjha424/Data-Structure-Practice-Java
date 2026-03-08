f182217f1b96b5495e0ed856e024d8aeadfbb9e2
rohitjha424 <rohitjha424@gmail.com>
2026-03-09 02:13:59 +0530

    endsWith() implemented

--- New2026/String/com/string/StringBasics.java
+++ New2026/String/com/string/StringBasics.java
@@ -63,6 +63,10 @@ public class StringBasics {
 	   //startsWith()>> will return if a string starts with given string
 	   System.out.println(str.startsWith("DS")); //true >> as "DSA With Java"; startsWith >>"DS"
 	   System.out.println(str.startsWith("MS")); //flase >> as "DSA With Java"; does not startsWith >>"MS"
+	   
+	 //endsWith()>> will return if a string ends with given string
+	   System.out.println(str.endsWith("ava")); //true >> as "DSA With Java"; endsWith >>"ava"
+	   System.out.println(str.endsWith("wor")); //flase >> as "DSA With Java"; does not endsWith >>"wor"
     
 
 }
