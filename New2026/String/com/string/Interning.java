708970266739f4940bed407b25444374e17d514a
rohitjha424 <rohitjha424@gmail.com>
2026-03-10 23:40:08 +0530

    Formatting

--- New2026/String/com/string/Interning.java
+++ New2026/String/com/string/Interning.java
@@ -1,26 +1,28 @@
 package com.string;
+
 import java.util.*;
+
 public class Interning {
-	
+
 	public static void main(String[] args) {
-		
+
 		String s = "Hello";
-		String x = "Hello"; 
-		
-		//Technically String s and x both are pointing to are String in Heap >> called Interning
-		
+		String x = "Hello";
+
+		// Technically String s and x both are pointing to are String in Heap >> called
+		// Interning
+
 		x = "Mello"; // will create new String >>"Mello" and x will point to it
-		
-		
-		//if you need to create a new String with same String characters>> then use new
-		
-		String y = new String("Hello"); //will create a new String 
-		
-		
-		//immutability of Strings in Java
-		
-		//s.charAt(1) = 'D'; // >> will throw Error>> Strings characters cannot be modified
-		
+
+		// if you need to create a new String with same String characters>> then use new
+
+		String y = new String("Hello"); // will create a new String
+
+		// immutability of Strings in Java
+
+		// s.charAt(1) = 'D'; // >> will throw Error>> Strings characters cannot be
+		// modified
+
 	}
 
 }
