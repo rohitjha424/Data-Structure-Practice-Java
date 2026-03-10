41e842bf6aee4aafa2dd7c149d60525b5a7e7627
rohitjha424 <rohitjha424@gmail.com>
2026-03-10 22:20:15 +0530

    more on concatination

---
+++ New2026/String/com/string/StringIntChar.java
@@ -0,0 +1,14 @@
+package com.string;
+import java.util.*;
+
+public class StringIntChar {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+//		String str = "abc";
+////		str = str+"xyz";
+//		str += 10;
+		System.out.println("abc"+10+20); // >> will print abc1020>> abc+10= abc10 then abc10+20>>abc1020
+		
+	}
+}
