d24a526eb9413969cf0b630501164c62f4747263
rohitjha424 <rohitjha424@gmail.com>
2026-03-10 22:25:10 +0530

    String+char

--- New2026/String/com/string/StringIntChar.java
+++ New2026/String/com/string/StringIntChar.java
@@ -5,10 +5,15 @@ public class StringIntChar {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-//		String str = "abc";
-////		str = str+"xyz";
+		String str = "abc";
+//		str = str+"xyz";
+		System.out.println(str += 'r'); // 'r' converted to string then concatenated with string
 //		str += 10;
 		System.out.println("abc"+10+20); // >> will print abc1020>> abc+10= abc10 then abc10+20>>abc1020
+		
+		
+
+		
 		
 	}
 }
