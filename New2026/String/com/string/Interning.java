e0492e7172a1ce787dc4d85e1e294f1fc888cbce
rohitjha424 <rohitjha424@gmail.com>
2026-03-10 23:27:36 +0530

    Creating new String with new

--- New2026/String/com/string/Interning.java
+++ New2026/String/com/string/Interning.java
@@ -12,6 +12,9 @@ public class Interning {
 		x = "Mello"; // will create new String >>"Mello" and x will point to it
 		
 		
+		//if you need to create a new String with same String characters>> then use new
+		
+		String y = new String("Hello"); //will create a new String 
 		
 	}
 
