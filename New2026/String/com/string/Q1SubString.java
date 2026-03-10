e9bf7358c3ac0b77f3beed5962f93be0cc0ff1f5
rohitjha424 <rohitjha424@gmail.com>
2026-03-10 22:38:34 +0530

    Q 1 Substring Solved

---
+++ New2026/String/com/string/Q1SubString.java
@@ -0,0 +1,25 @@
+//Q1) What will be the o/p of the given code below 
+
+package com.string;
+
+public class Q1SubString {
+
+
+		public static void main(String[] args) {
+			String s = "physics";
+			
+			for(int j = 2; j<4; j++) {
+				System.out.print(s.substring(j));
+			}
+			
+			
+			
+		
+	}
+}
+
+/* O/P:
+ 
+ysicssics
+
+*/
