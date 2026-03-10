12a03926f94f479454c3861c3b1891ab0f501b26
rohitjha424 <rohitjha424@gmail.com>
2026-03-10 22:29:18 +0530

    substring(i,j) implemented

---
+++ New2026/String/com/string/SubString.java
@@ -0,0 +1,13 @@
+package com.string;
+
+import java.util.*;
+
+public class SubString {
+
+	public static void main(String[] args) {
+		String str = "abcd";
+		
+		//substring(i,j)
+		System.out.println(str.substring(0,2)); //will print from 0th index to 2-1 =1 index.>>substring(i,j)>>j is excluded>>j-1 is included
+	}
+}
