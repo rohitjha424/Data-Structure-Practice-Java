99a358bc39db21c3d7ed8c9a288fbfc89bb5bd4a
rohitjha424 <rohitjha424@gmail.com>
2026-03-12 03:50:51 +0530

    More on append()

--- New2026/String/com/string/Builder.java
+++ New2026/String/com/string/Builder.java
@@ -9,24 +9,30 @@ public class Builder {
 		//StringBuilder Functions
 		
 		//append()>>> to concatenate the string at the end
-//		str.append("World");
+//		str.append("World"); //appending String
 //		System.out.println(str); // will print "helloWorld"
 		
+		str.append(3); // appending integer
+		System.out.println(str); // will print "hello3"
+		
+		str.append('t'); //appending char
+		System.out.println(str); // will print "hello3t"
+		
 		//setCharAt()>> used to modify the character of a string at particular index
-		str.setCharAt(0, 'm');
-		System.out.println(str); // will print "mello"
-		
-		
-		//insert()>>will insert character within the String>>length will also increase
-		
-		str.insert(2, 'y');// inserting char
-		System.out.println(str); // will print "meyllo"
-		
-		str.insert(1, 5); // inserting integer
-		System.out.println(str); // will print "m5eyllo"
-		
-		str.insert(3, "Hey"); //inserting String
-		System.out.println(str); // will print "m5eHeyyllo"
+//		str.setCharAt(0, 'm');
+//		System.out.println(str); // will print "mello"
+//		
+//		
+//		//insert()>>will insert character within the String>>length will also increase
+//		
+//		str.insert(2, 'y');// inserting char
+//		System.out.println(str); // will print "meyllo"
+//		
+//		str.insert(1, 5); // inserting integer
+//		System.out.println(str); // will print "m5eyllo"
+//		
+//		str.insert(3, "Hey"); //inserting String
+//		System.out.println(str); // will print "m5eHeyyllo"
 		
 		
 	}
