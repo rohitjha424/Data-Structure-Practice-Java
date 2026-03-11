034fbb30a3c4cc60aaab6bdc3ce60f6406115923
rohitjha424 <rohitjha424@gmail.com>
2026-03-12 03:37:25 +0530

    append() implemented

--- New2026/String/com/string/Builder.java
+++ New2026/String/com/string/Builder.java
@@ -5,7 +5,12 @@ import java.util.*;
 public class Builder {
 	public static void main(String[] args) {
 		StringBuilder str = new StringBuilder("hello");
-
+		
+		//StringBuilder Functions
+		
+		//append()>>> to concatenate the string at the end
+		str.append("World");
+		System.out.println(str); // will print "helloWorld"
 	}
 
 }
