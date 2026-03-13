c43d6d3a767654e2cbe9b6a3f7f481615560550e
rohitjha424 <rohitjha424@gmail.com>
2026-03-13 23:49:38 +0530

    Space condition taken care

--- New2026/String/com/string/Q3StringToggle.java
+++ New2026/String/com/string/Q3StringToggle.java
@@ -16,6 +16,10 @@ public class Q3StringToggle {
 		for (int i = 0; i < str.length(); i++) {
 			boolean capFlag = true; // true for Capital
 			char ch = str.charAt(i); // getting Char At i
+			
+			if(ch==' ')
+			continue;
+			
 			int ascii = (int) ch; // finding Ascii value of Character
 			if (ascii >= 97) // smallCase
 				capFlag = false; // for smallCase Setting as False
