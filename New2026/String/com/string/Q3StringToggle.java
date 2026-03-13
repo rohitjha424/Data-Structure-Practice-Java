f8b3bc36b616d710e19a1151047ae1b7526e9cb6
rohitjha424 <rohitjha424@gmail.com>
2026-03-13 23:38:26 +0530

    Comments Added

--- New2026/String/com/string/Q3StringToggle.java
+++ New2026/String/com/string/Q3StringToggle.java
@@ -14,21 +14,22 @@ public class Q3StringToggle {
 
 		// toggle
 		for (int i = 0; i < str.length(); i++) {
-			boolean capFlag = true;
-			char ch = str.charAt(i);
-			int ascii = (int) ch;
-			if (ascii >= 97)
-				capFlag = false;
+			boolean capFlag = true; // true for Capital
+			char ch = str.charAt(i); // getting Char At i
+			int ascii = (int) ch; // finding Ascii value of Character
+			if (ascii >= 97) // smallCase
+				capFlag = false; // for smallCase Setting as False
+
 			if (capFlag == true) {
-				ascii = ascii + 32;
-				char c = (char) ascii;
-				str.setCharAt(i, c);
+				ascii = ascii + 32; // 32 is difference between each Upper Case and Lowercase chars in Ascii
+				char c = (char) ascii; // getting char corresponding to ASCII value
+				str.setCharAt(i, c); // setting the toggle Char
 			} else {
+				// Lower to upperCase
 				ascii = ascii - 32;
 				char c = (char) ascii;
 				str.setCharAt(i, c);
 			}
-
 		}
 		System.out.println(str);
 
