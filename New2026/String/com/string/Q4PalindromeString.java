6cc3deb394db8b319bc417efafd7b5187ef7048c
rohitjha424 <rohitjha424@gmail.com>
2026-03-15 00:07:35 +0530

    Solved without using pre-built reverse() method

--- New2026/String/com/string/Q4PalindromeString.java
+++ New2026/String/com/string/Q4PalindromeString.java
@@ -6,14 +6,37 @@ public class Q4PalindromeString {
 		String str = "abcdcba";
 		
 		//using reverse() String Builder pre-built Method
-		StringBuilder gtr = new StringBuilder(str); // String to StringBuiler for using its functions
-		gtr.reverse(); // reversing String
-		String s = gtr+""; // adding empty String to StringBuilder will convert it back to String
-		if(str.equals(s)) {
-			System.out.println("Palindrome String !!!");
+//		StringBuilder gtr = new StringBuilder(str); // String to StringBuiler for using its functions
+//		gtr.reverse(); // reversing String
+//		String s = gtr+""; // adding empty String to StringBuilder will convert it back to String
+//		if(str.equals(s)) {
+//			System.out.println("Palindrome String !!!");
+//		}
+//		else {
+//			System.out.println(" Not a Palindrome String!!!");
+//		}
+		
+		// Suing for Loop
+		
+		int i=0;
+		int j = str.length()-1;
+		boolean isPalindrome= true;
+		while(i<j) {
+			if(str.charAt(i) == str.charAt(j)) {
+				i++;
+				j--;
+			}
+			else {
+				isPalindrome =false;
+				break;
+			}
+		
+		}
+		if(isPalindrome==true) {
+			System.out.println("Palindrome String!!!");
 		}
 		else {
-			System.out.println(" Not a Palindrome String!!!");
+			System.out.println("Not a Palindrome String!!!");
 		}
 	}
 }
