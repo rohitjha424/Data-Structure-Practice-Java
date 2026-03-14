1b0130e09518b0a76763bdeaa12dbc8ce27061d0
rohitjha424 <rohitjha424@gmail.com>
2026-03-14 23:54:55 +0530

    Comments Added

--- New2026/String/com/string/Q4PalindromeString.java
+++ New2026/String/com/string/Q4PalindromeString.java
@@ -4,9 +4,9 @@ public class Q4PalindromeString {
 
 	public static void main(String[] args) {
 		String str = "abcdcba";
-		StringBuilder gtr = new StringBuilder(str);
-		gtr.reverse();
-		String s = gtr+"";
+		StringBuilder gtr = new StringBuilder(str); // String to StringBuiler for using its functions
+		gtr.reverse(); // reversing String
+		String s = gtr+""; // adding empty String to StringBuilder will convert it back to String
 		if(str.equals(s)) {
 			System.out.println("Palindrome String !!!");
 		}
