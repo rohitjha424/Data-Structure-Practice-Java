3c52cf5fe8a0bcaf04280ac68423d8b3966e653b
rohitjha424 <rohitjha424@gmail.com>
2026-03-16 23:32:45 +0530

    Solved Using Backtracking of String

--- New2026/String/com/string/questions/Q6ReverseString.java
+++ New2026/String/com/string/questions/Q6ReverseString.java
@@ -1,20 +1,34 @@
 package com.string.questions;
 
 public class Q6ReverseString {
-
+	
+	
 	public static void main(String[] args) {
 		String s = "physics";
 
-        //As String doesn't have in built method reverse()>> hence converting it to StringBuilder
-		StringBuilder sb = new StringBuilder(s);
-
-		// using in-built method >> reverse()
-		sb.reverse();
-
-        //converting StringBuilder back to String 
-        String result = sb.toString();
-        
-		System.out.println(result); // will print : "scisyhp"
+//        //As String doesn't have in built method reverse()>> hence converting it to StringBuilder
+//		StringBuilder sb = new StringBuilder(s);
+//
+//		// using in-built method >> reverse()
+//		sb.reverse();
+//
+//        //converting StringBuilder back to String 
+//        String result = sb.toString();
+//        
+//		System.out.println(result); // will print : "scisyhp"
+		
+		
+		//Without Using in built function
+		
+		//1) applying Backtracking of String 
+		
+		String reversed = "";
+		
+		for(int i = s.length()-1; i>=0; i-- ) {
+			reversed = reversed+ s.charAt(i);
+		}
+		System.out.println(reversed); // will print : "scisyhp"
+		
 
 	}
 }
