2f4e433ae63d576af4d641bd6992408267eb93f9
rohitjha424 <rohitjha424@gmail.com>
2026-03-17 03:58:40 +0530

    Solved

--- New2026/String/com/string/questions/Q8CompressDuplicateCharacters.java
+++ New2026/String/com/string/questions/Q8CompressDuplicateCharacters.java
@@ -7,4 +7,37 @@ package com.string.questions;
 
 public class Q8CompressDuplicateCharacters {
 
+	public static void main(String[] args) {
+		String str = "aaabbbbccddde";
+		StringBuilder sb = new StringBuilder(); 
+
+		int j;
+		for (int i = 0; i < str.length(); i++) {
+			int count = 1;
+			
+			for (j = i+1; j < str.length(); j++) {
+
+				if (str.charAt(i) == str.charAt(j)) {
+					count++;
+				}
+				else {
+					sb.append(str.charAt(i));
+					i=j-1;
+					break;
+				}
+			}
+			
+	
+			if(count>1) {
+				sb.append(count);
+			}
+			if(j==str.length()-1 && (str.charAt(j) != str.charAt(j-1))) {
+				sb.append(str.charAt(j));
+			}
+
+		}
+		System.out.println(sb);
+
+	}
+
 }
