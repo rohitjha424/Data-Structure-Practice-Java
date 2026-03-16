765f3c3d87741b10e3a1ca25b4ba2bdc4931937c
rohitjha424 <rohitjha424@gmail.com>
2026-03-17 00:23:10 +0530

    Solved

--- New2026/String/com/string/questions/Q7ReverseEachWordOfString.java
+++ New2026/String/com/string/questions/Q7ReverseEachWordOfString.java
@@ -2,10 +2,39 @@
 Example :
 	input: "I am an Engineer"
 	output: "I ma na reenignE"
-*/		
+*/
 
 package com.string.questions;
 
 public class Q7ReverseEachWordOfString {
 
+	public static void main(String[] args) {
+
+		String str = "I am an Engineer ";
+
+		// Approach >> take one word at a time store in SStringBuilder and reverse it
+		// and Store in Ans.
+
+		// keeping ans and String builder empty
+		String ans = "";
+		StringBuilder sb = new StringBuilder("");
+
+		int i = 0;
+		for (int j = 0; j < str.length(); j++) {
+			//checking when Space (a word ends)
+			if (str.charAt(j) == ' ') {
+				sb = new StringBuilder(str.substring(i, j)); 	// taking that Word and Storing to SB
+				i = j + 1; 										// moving initial pointer to next word.
+				sb.reverse(); 									// reversing string builder word.
+				ans = ans + sb.toString() + " ";				//Storing reversed word to ans with Space
+				sb = new StringBuilder("");						//emptying SB.
+			}
+		}
+
+		// removing Extra Space
+		String finalOutput = ans.trim();
+
+		System.out.println(finalOutput);
+
+	}
 }
