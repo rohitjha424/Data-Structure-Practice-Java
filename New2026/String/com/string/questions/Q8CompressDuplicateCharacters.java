78f0a0a8277d965a14fd5b5f45c0dc10d1dd58f4
rohitjha424 <rohitjha424@gmail.com>
2026-03-17 03:09:34 +0530

    CompressDuplicateCharacters Started

---
+++ New2026/String/com/string/questions/Q8CompressDuplicateCharacters.java
@@ -0,0 +1,10 @@
+/*Q8 Compress Duplicate characters of the String followed by the number of consecutive duplicates
+Example :
+	input: "aaabbbbccddde"
+	output: "a3b4c2d3e"
+*/
+package com.string.questions;
+
+public class Q8CompressDuplicateCharacters {
+
+}
