55da09aaf0f934b7fc2cdbc0ab16b70a108ce0d9
rohitjha424 <rohitjha424@gmail.com>
2026-03-16 22:33:31 +0530

    little Correction

--- New2026/String/com/string/questions/Q6ReverseString.java
+++ New2026/String/com/string/questions/Q6ReverseString.java
@@ -5,11 +5,16 @@ public class Q6ReverseString {
 	public static void main(String[] args) {
 		String s = "physics";
 
-		// using in-built method >> reverse()
+        //As String doesn't have in built method reverse()>> hence converting it to StringBuilder
 		StringBuilder str = new StringBuilder(s);
+
+		// using in-built method >> reverse()
 		str.reverse();
 
-		System.out.println(str); // will print : "scisyhp"
+        //converting StringBuilder back to String 
+        String result = str.toString();
+        
+		System.out.println(result); // will print : "scisyhp"
 
 	}
 }
