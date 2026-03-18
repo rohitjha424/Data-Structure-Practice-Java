6006966fd3ca420b12e2d753449d57f84a2de184
rohitjha424 <rohitjha424@gmail.com>
2026-03-18 23:53:07 +0530

    Attempting checkIfRotation Question

---
+++ New2026/String/com/string/questions/Q9CheckforRotation.java
@@ -0,0 +1,24 @@
+/*Q9: Check if Strings Are Rotations of Each Other
+Input: s1 = "abcd", s2 = "cdab"
+Output: true
+Explanation: After 2 right rotations, s1 will become equal to s2.
+
+*/
+
+package com.string.questions;
+
+public class Q9CheckforRotation {
+	
+	//checkIfRotation
+	public static boolean checkIfRotation(String str) {
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String str = "abcd";
+		
+		checkIfRotation(str);
+		
+	}
+
+}
