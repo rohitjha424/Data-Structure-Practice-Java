3b40735f74857daf4ee70ed98fb6220abd25dcec
rohitjha424 <rohitjha424@gmail.com>
2026-03-20 22:30:06 +0530

    Formatting and Comments Added

--- New2026/String/com/string/questions/Q6ReverseString.java
+++ New2026/String/com/string/questions/Q6ReverseString.java
@@ -1,57 +1,55 @@
 package com.string.questions;
 
 public class Q6ReverseString {
-	
-	
+
 	public static void main(String[] args) {
 		String s = "physics";
 
-//        //As String doesn't have in built method reverse()>> hence converting it to StringBuilder
+//	// 	 Method 1) using in-built StrinBuilder method >> reverse()
+		
+//      //As String doesn't have in built method reverse()>> hence converting it to StringBuilder
 //		StringBuilder sb = new StringBuilder(s);
 //
-//		// using in-built method >> reverse()
 //		sb.reverse();
 //
-//        //converting StringBuilder back to String 
-//        String result = sb.toString();
+//      //converting StringBuilder back to String 
+//      String result = sb.toString();
 //        
 //		System.out.println(result); // will print : "scisyhp"
 		
 		
-		//Without Using in built function
-		
-		//1) applying Backtracking of String 
-		
+
+		// Method 2) Without Using in built function
+
+		// 1) applying Backtracking of String
+
 //		String reversed = "";
 //		
 //		for(int i = s.length()-1; i>=0; i-- ) {
 //			reversed = reversed+ s.charAt(i);
 //		}
 //		System.out.println(reversed); // will print : "scisyhp"
-		
+
 //		
-		//2) using 2 pointers Approach 
-		
-		//here we will use char[] array to swap the first and last chars of string 
+		// 2) using 2 pointers Approach
+
+		// here we will use char[] array to swap the first and last chars of string
 		char[] charArray = s.toCharArray();
-		
+
 		int i = 0;
-		int j = charArray.length-1;
-		
-		while (i<j) {
-			
+		int j = charArray.length - 1;
+
+		while (i < j) {
+
 			char temp = charArray[i];
 			charArray[i] = charArray[j];
-			charArray[j]= temp;
-			
+			charArray[j] = temp;
+
 			i++;
 			j--;
 		}
 		String reversedString = new String(charArray);
 		System.out.println(reversedString);
-		
-		
-		
 
 	}
 }
